e39c835ba986ca22a7500241d2203c479a0b692e
meharryp <harryhahahpollard@aol.co.uk>
2016-07-12 15:26:44 +0100

    Economy handles nill money correctly
    Before this new players would be unable to gain money until someone set
    it to a number.

--- gamemode/core/economy/economy_sh.lua
+++ gamemode/core/economy/economy_sh.lua
@@ -1,15 +1,13 @@
 local Player = FindMetaTable('Player')
 
-
 -- Player:canAfford(amount:number)
 -- @ret canAfford:bool - can the player afford the amount requested
 function Player:canAfford(amount)
-	return self:getMoney() > amount
+	return self:getMoney() or 0 > amount
 end
 
 -- Player:getMoney()
 -- @ret amount:number player's current balance
 function Player:getMoney()
-	return self:GetFWData().money
+	return self:GetFWData().money or 0
 end
-
