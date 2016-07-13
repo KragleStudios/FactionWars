0fff83d705ffad0877e693443c6923d415e18dae
meharryp <harryhahahpollard@aol.co.uk>
2016-07-13 17:59:44 +0100

    Additional validation on setMoney
    Should stop people from setting money to a negative value.

--- gamemode/core/economy/economy_sv.lua
+++ gamemode/core/economy/economy_sv.lua
@@ -7,6 +7,7 @@ fw.data.addPersistField('money')
 -- @ret amount:number same as the amount set
 function Player:setMoney(amount)
 	if type(amount) ~= 'number' then error("player money must be a number") end
+	if amount < 0 then error("value must be positive") end
 	self:GetFWData().money = amount
 	return amount
 end
