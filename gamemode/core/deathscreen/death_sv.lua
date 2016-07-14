77b92d81dbd7d7e23fe38de3c20304f21203abba
Steven <FlutteralYoshhi@hotmail.com>
2016-07-13 21:42:26 -0700

    Update death_sv.lua

--- gamemode/core/deathscreen/death_sv.lua
+++ gamemode/core/deathscreen/death_sv.lua
@@ -1,10 +1,10 @@
 util.AddNetworkString("playerDeath")
 
 hook.Add("PlayerDeath", "Death", function (victim, inflictor, attacker)
-    local bool = (victim == attacker) or attacker:IsWorld() or (attacker:GetClass() == "prop_physics") or false
+        local bool = (victim == attacker) or attacker:IsWorld() or (attacker:GetClass() == "prop_physics") or false
 
-    net.Start("playerDeath")
-        net.WriteBool(bool)
-	      net.WriteEntity(attacker)
-	  net.Send(victim)
+        net.Start("playerDeath")
+                net.WriteBool(bool)
+	        net.WriteEntity(attacker)
+	net.Send(victim)
 end)
