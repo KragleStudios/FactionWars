19a11fff67fb62530e44f862f26da9303cc20b7c
Harry Pollard <harrysmod69@gmail.com>
2016-07-15 09:11:47 +0100

    Update shared.lua

--- entities/entities/fw_shipment/shared.lua
+++ entities/entities/fw_shipment/shared.lua
@@ -3,6 +3,7 @@ ENT.Base = "base_entity"
 
 ENT.Name = "Shipment"
 
-function ENT:getRemaining()
-	return self:GetNWInt("remaining", 0)
+function ENT:SetupDataTables()
+	self:NetworkVar("Int", 0, "Remaining")
+	self:NetworkVar("String", 1, "Name")
 end
