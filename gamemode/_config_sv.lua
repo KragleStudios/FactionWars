611b2e49c4c097e7a43b2cd9b9f826821d685143
sanny <sannysherief@gmail.com>
2016-07-18 00:07:53 -0700

    Started SQL compatibility

--- gamemode/_config_sv.lua
+++ gamemode/_config_sv.lua
@@ -2,7 +2,14 @@ fw.config = fw.config or {} -- for now. todo: make into a module
 
 fw.config.dataDir = 'factionwars'
 
-fw.config.dataStore = 'text' -- text documents
+fw.config.sql = {
+	host = '',
+	database = '',
+	username = '',
+	password = '',
+	module = 'sqlite',
+}
+
 
 fw.config.data_cacheUpdateInterval = 60 -- SECONDS
 fw.config.data_storeUpdateInterval = 60 * 10 -- SECONDS
