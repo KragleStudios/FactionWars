0909e89741f63886eac0d8b332ccbdd65cc99fdb
meharryp <harryhahahpollard@aol.co.uk>
2016-07-19 14:40:49 +0100

    Added commands to reload printers and guns
    fw_reloadguns
    fw_reloadprinters

--- gamemode/core/printers/module.lua
+++ gamemode/core/printers/module.lua
@@ -8,3 +8,8 @@ fw.printers = fw.printers or {}
 
 fw.include_sh 'printers_sh.lua'
 fw.include_sh 'printers.lua'
+
+concommand.Add("fw_reloadprinters", function(ply, cmd, args)
+	if IsValid(ply) and not ply:IsSuperAdmin() then pl:ChatPrint('insufficient privliages') return end
+	fw.hook.GetTable("Initialize").LoadPrinters()
+end)
--- gamemode/core/weapons/module.lua
+++ gamemode/core/weapons/module.lua
@@ -13,3 +13,8 @@ fw.weapons.SLOT_RIFLE = 4
 
 fw.include_sh 'weapons_sh.lua'
 fw.include_sh 'weapons.lua'
+
+concommand.Add("fw_reloadguns", function(ply, cmd, args)
+	if IsValid(ply) and not ply:IsSuperAdmin() then pl:ChatPrint('insufficient privliages') return end
+	fw.hook.GetTable("Initialize").LoadWeapons()
+end)
