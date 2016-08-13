7ec70bb982eb5364607ec96b1f568e9db7465881
krekeris <alxkrekeris@gmail.com>
2016-08-14 00:45:10 +0300

    made ui use 3d2d lib

--- entities/entities/fw_respawn_point/cl_init.lua
+++ entities/entities/fw_respawn_point/cl_init.lua
@@ -1,24 +1,45 @@
 include("shared.lua")
 
+function ENT:InitializeVGUI()
+	if IsValid(self._panel) then return end 
+
+	local owner = self:FWGetOwner()
+	local text = IsValid(owner) and (owner:Name() .. "'s") or "unknown's"
+	local nickFont = fw.fonts.default_compact_shadow:fitToView(566, 75, text)
+	local spawnpFont = fw.fonts.default_compact_shadow:fitToView(566, 75, "Spawn Point")
+	local color = IsValid(owner) and fw.team.factions[owner:getFaction()].color or color_white
+
+	local pnl = vgui.Create("DPanel")
+	pnl:SetSize(580, 150)
+	pnl.Paint = function(pnl, w, h)
+		surface.SetDrawColor(40, 40, 50, 200)
+		surface.DrawRect(0, 0, w, h)
+
+		draw.SimpleText(text, nickFont, w / 2, 75 / 2, color, TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER)
+		draw.SimpleText("Spawn Point", spawnpFont, w / 2, 75 + 75 / 2, color, TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER)
+
+		surface.SetDrawColor(color)
+		surface.DrawOutlinedRect(0, 0, w, h, color.r, color.g, color.b, 100)
+		surface.DrawOutlinedRect(1, 1, w - 2, h - 2, color.r, color.g, color.b, 100)
+	end
+
+	self._panel = pnl
+end
+
 function ENT:Draw()
 	self:DrawModel()
-	if (LocalPlayer():GetPos():DistToSqr(self:GetPos()) > 1000 * 1000) then return end
 
-	local own = self:FWGetOwner()
+	if not IsValid(self._panel) then return end
 
-	local font = fw.fonts.default_compact_shadow:atSize(100)
-	local text = (IsValid(own) and own:Nick() or "owner") .. "'s "
-	local text2 = "Spawn Point"
-	local col = own:IsPlayer() and fw.team.factions[own:getFaction()].color or Color(255, 255, 255)
-
-	surface.SetFont(font)
-	local x = surface.GetTextSize(text)
-	local x2, y = surface.GetTextSize(text2)
-
-	local eye = LocalPlayer():EyeAngles()
-
-	cam.Start3D2D(self:GetPos() + self:GetUp() * 50, Angle(0, eye.y - 90, 90), .05)
-	    draw.SimpleTextOutlined(text, font, -(x / 2), 0, col, 0, 0, 0.5, Color(0, 0, 0, 255))
-	    draw.SimpleTextOutlined(text2, font, -(x2 / 2), y, col, 0, 0, 0.5, Color(0, 0, 0, 255))
-	cam.End3D2D()
+	self._panel:Draw3D(self:GetPos() + self:GetUp() * 45, Angle(0, LocalPlayer():EyeAngles().y - 90, 90), 0.05)
 end
+
+function ENT:Initialize()
+	self:InitializeVGUI()
+end
+
+function ENT:OnRemove()
+	if IsValid(self._panel) then
+		self._panel:Remove()
+	end
+end  
