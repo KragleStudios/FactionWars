2ac61862c31b3b936a3a58a01c77c6d768538f37
Steven <FlutteralYoshhi@hotmail.com>
2016-08-12 21:01:33 -0700

    Removed dumb fonts and replaced dank default fonts, slight design change
    lastest version

--- entities/entities/fw_healthmachine/cl_init.lua
+++ entities/entities/fw_healthmachine/cl_init.lua
@@ -1,10 +1,9 @@
-
 include("shared.lua")
-surface.CreateFont("ChargeFont", {font = "OpenSans", size = 44, weight = 500})
-surface.CreateFont("HealthSign", {font = "OpenSans", size= 72, weight  = 900})
 
 function ENT:Draw()
 	self:DrawModel()
+
+	local font = fw.fonts.default_compact:atSize(45)
 
 	local ang = self:GetAngles()
 	ang:RotateAroundAxis(self:GetUp(), 90)
@@ -12,36 +11,40 @@ function ENT:Draw()
 
 	cam.Start3D2D(self:GetPos() + self:GetUp() * 28 + self:GetForward() * 13 + self:GetRight() * 7.5, ang, .1)
 		surface.SetDrawColor(32,32,32,235)
-			surface.DrawRect(0,0,235,77)
+		surface.DrawRect(0,0,235,77)
 
 		surface.SetDrawColor(100 - (self:GetCharge() / self:GetMaxCharge()) * 255, (self:GetCharge() / self:GetMaxCharge()) * 255, 0)
-			surface.DrawRect(1,1, (self:GetCharge() / self:GetMaxCharge()) * 233, 35  )
+		surface.DrawRect(1,1, (self:GetCharge() / self:GetMaxCharge()) * 233, 35)
 
 		if self:GetCharge() <= 0 then 
 			surface.SetDrawColor(192, 57, 43)
-				surface.DrawRect(1,1, 233, 35)
+			surface.DrawRect(1,1, 233, 35)
 		end
 
 		local txt = "Charge: " .. self:GetCharge() or "#Error"
-		surface.SetFont("ChargeFont")
+		local txt2 = "HEALTH"
+
+		surface.SetFont(font)
 		surface.SetTextColor(Color(255,255,255))
 		surface.SetTextPos(117.5 - surface.GetTextSize(txt) * .5,32)
-			surface.DrawText(txt)
-
-		surface.SetDrawColor(0,0,0,255)
-			surface.DrawOutlinedRect(0,0,235,77)
-			surface.DrawOutlinedRect(0,0,235,37)
+		surface.DrawText(txt)
 
 		surface.SetDrawColor(32,32,32)
-			surface.DrawRect(80,80, 75,47)	
+		surface.DrawRect(0,80,235,35)	
 
-		surface.SetFont("HealthSign")
-		surface.SetTextPos(117.5 - surface.GetTextSize("+") * .5, 60)
-			surface.DrawText("+")
+		surface.SetDrawColor(0,0,0,255)
+		surface.DrawOutlinedRect(0,80,235,35)
 
+		surface.SetFont(font)
+		surface.SetTextColor(Color(231, 76, 60))
+		surface.SetTextPos(117.5 - surface.GetTextSize(txt2) * .5, 75)
+		surface.DrawText(txt2)
+
+		surface.SetDrawColor(0,0,0,255)
+		surface.DrawOutlinedRect(0,0,235,77)
+		surface.DrawOutlinedRect(0,0,235,37)
 	cam.End3D2D()
 end
 
 function ENT:Initialize()
-
 end
