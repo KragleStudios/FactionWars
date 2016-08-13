c1e909da737dedb78b448b7087896d34c7b1c354
Steven <FlutteralYoshhi@hotmail.com>
2016-08-12 21:09:55 -0700

    Higher quality text, removed useless spinning and faces player
    lastest version

--- entities/entities/fw_respawn_point/cl_init.lua
+++ entities/entities/fw_respawn_point/cl_init.lua
@@ -6,7 +6,7 @@ function ENT:Draw()
 
 	local own = self:FWGetOwner()
 
-	local font = fw.fonts.default:atSize(30)
+	local font = fw.fonts.default_compact_shadow:atSize(100)
 	local text = (IsValid(own) and own:Nick() or "owner") .. "'s "
 	local text2 = "Spawn Point"
 	local col = own:IsPlayer() and fw.team.factions[own:getFaction()].color or Color(255, 255, 255)
@@ -15,15 +15,15 @@ function ENT:Draw()
 	local x = surface.GetTextSize(text)
 	local x2, y = surface.GetTextSize(text2)
 
-	local rotation = CurTime() * 11
+	local eye = LocalPlayer():EyeAngles()
 
-	cam.Start3D2D(self:GetPos() + self:GetUp() * 90, Angle(0, rotation, 90), .25)
-		draw.DrawText(text, font, -(x / 2), 0, col)
-		draw.DrawText(text2, font, -(x2 / 2), y, col)
+	cam.Start3D2D(self:GetPos() + self:GetUp() * 90, Angle(0, eye.y - 90, 90), .05)
+	    draw.SimpleTextOutlined(text, font, -(x / 2), 0, col, 0, 0, 0.5, Color(0, 0, 0, 255))
+	    draw.SimpleTextOutlined(text2, font, -(x2 / 2), y, col, 0, 0, 0.5, Color(0, 0, 0, 255))
 	cam.End3D2D()
 
-	cam.Start3D2D(self:GetPos() + self:GetUp() * 90, Angle(0, rotation + 180, 90), .25)
-		draw.DrawText(text, font, -(x / 2), 0, col)
-		draw.DrawText(text2, font, -(x2 / 2), y, col)
+	cam.Start3D2D(self:GetPos() + self:GetUp() * 90, Angle(0, eye.y - 90, 90), .05)
+		draw.SimpleTextOutlined(text, font, -(x / 2), 0, col, 0, 0, 0.5, Color(0, 0, 0, 255))
+		draw.SimpleTextOutlined(text2, font, -(x2 / 2), y, col, 0, 0, 0.5, Color(0, 0, 0, 255))
 	cam.End3D2D()
 end
