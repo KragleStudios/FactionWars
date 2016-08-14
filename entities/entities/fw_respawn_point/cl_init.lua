40879cf61e0c905bfc9a5fc79815ec581713170c
krekeris <alxkrekeris@gmail.com>
2016-08-14 10:36:09 +0300

    changed ui once again

--- entities/entities/fw_respawn_point/cl_init.lua
+++ entities/entities/fw_respawn_point/cl_init.lua
@@ -1,46 +1,35 @@
 include("shared.lua")
 
-function ENT:InitializeVGUI()
-	if IsValid(self._panel) then return end 
-
-	local owner = self:FWGetOwner()
-	local text = IsValid(owner) and (owner:Name() .. "'s") or "unknown's"
-	local nickFont = fw.fonts.default_compact_shadow:fitToView(566, 75, text)
-	local spawnpFont = fw.fonts.default_compact_shadow:fitToView(566, 75, "Spawn Point")
-	local color = IsValid(owner) and fw.team.factions[owner:getFaction()].color or color_white
-
-	local pnl = vgui.Create("DPanel")
-	pnl:SetSize(580, 150)
-	pnl.Paint = function(pnl, w, h)
-		surface.SetDrawColor(40, 40, 50, 200)
-		surface.DrawRect(0, 0, w, h)
-
-		draw.SimpleText(text, nickFont, w / 2, 75 / 2, color, TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER)
-		draw.SimpleText("Spawn Point", spawnpFont, w / 2, 75 + 75 / 2, color, TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER)
-
-		surface.SetDrawColor(color)
-		surface.DrawOutlinedRect(0, 0, w, h, color.r, color.g, color.b, 100)
-		surface.DrawOutlinedRect(1, 1, w - 2, h - 2, color.r, color.g, color.b, 100)
-	end
-
-	self._panel = pnl
+function ENT:GetDisplayPosition()
+	local obbcenter = self:OBBCenter()
+	local obbmax = self:OBBMaxs()
+	return Vector(obbcenter.x, obbmax.y - 19.5, obbcenter.z + 41), Angle(0, LocalPlayer():EyeAngles().y - 90, 90), 0.2
 end
 
 function ENT:Draw()
 	self:DrawModel()
-	if (LocalPlayer():GetPos():DistToSqr(self:GetPos()) > 1000 * 1000) then return end
-
-	if not IsValid(self._panel) then return end
-
-	self._panel:Draw3D(self:GetPos() + self:GetUp() * 45, Angle(0, LocalPlayer():EyeAngles().y - 90, 90), 0.05)
+	self:FWDrawInfo()
 end
 
-function ENT:Initialize()
-	self:InitializeVGUI()
-end
-
-function ENT:OnRemove()
-	if IsValid(self._panel) then
-		self._panel:Remove()
+function ENT:CustomUI(panel)
+	local header = vgui.Create("FWUITextBox", panel)
+	header:SetText("OWNER")
+	header:SetTall(18)
+	header:SetInset(1)
+	header:SetAlign("left")
+	header.Paint = function(self, w, h)
+		surface.SetDrawColor(0, 0, 0, 220)
+		surface.DrawRect(0, 0, w, h)
 	end
-end  
+
+	local owner = self:FWGetOwner()
+	local ownerName = vgui.Create("FWUITextBox", panel)
+	ownerName:SetText(IsValid(owner) and owner:Nick() or "Unknown")
+	ownerName:SetTall(18)
+	ownerName:SetInset(1)
+	ownerName:SetAlign("left")
+	ownerName.Paint = function(self, w, h)
+		surface.SetDrawColor(0, 0, 0, 220)
+		surface.DrawRect(0, 0, w, h)
+	end
+end
