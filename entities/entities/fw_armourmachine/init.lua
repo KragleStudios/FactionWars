05a6398dbeb8b9309760cf610d1877271c47e2a0
krekeris <alxkrekeris@gmail.com>
2016-08-18 18:30:03 +0300

    changed some stuff back

--- entities/entities/fw_armourmachine/init.lua
+++ entities/entities/fw_armourmachine/init.lua
@@ -14,23 +14,23 @@ function ENT:Initialize()
 	fw.resource.addEntity(self)
 
 	self.Consumes = {power = 1}
-	self:FWSetResource("armor", 5)
+	self.Produces = {armor = 5}
 
 	-- every minute add 1 armor
 	timer.Create("fw-armourmachine-refill-" .. self:EntIndex(), 60, 0, function()
 		if not IsValid(self) or self:FWHaveResource("power") < self.Consumes.power then return end
-		local armor = self:FWHaveResource("armor") or 0
-		self:FWSetResource("armor", math.min(armor + 1, self.MaxProduction.armor))
+		local armor = self.Produces.armor or 0
+		self.Produces.armor = math.min(armor + 1, self.MaxProduction.armor)
 	end)
 end
 
 function ENT:Use(ply)
 	if ply:Armor() < 100 then
-		local armor = self:FWHaveResource("armor") or 0
+		local armor = self.Produces.armor or 0
 		if armor > 0 then
 			ply:SetArmor(100)
 			self:EmitSound("hl1/fvox/boop.wav", 150, 100, 1, CHAN_AUTO)
-			self:FWSetResource("armor", armor - 1)
+			self.Produces.armor = armor - 1
 		else
 			self:EmitSound("hl1/fvox/buzz.wav", 150, 100, 1, CHAN_AUTO)
 		end
